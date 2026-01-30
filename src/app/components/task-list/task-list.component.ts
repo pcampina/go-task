2226daae1fcbfc56437e47906d569e3e9f90cd69
pcampina <pablo@campina.dev>
2026-01-30 17:48:39 +0000

    chore: task card markup

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TaskCardComponent } from "../task-card/task-card.component";
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [TaskCardComponent],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
