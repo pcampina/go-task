df540fad8798d5f9f0a8d46206b6d6f1e8165d69
pcampina <pablo@campina.dev>
2026-01-31 14:21:32 +0000

    chore: task form and comments markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { MainContentComponent } from "./components/main-content/main-content.component";
 import { TaskFormComponent } from "./components/task-form/task-form.component";
+import { TaskCommentsComponent } from "./components/task-comments/task-comments.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, MainContentComponent, TaskFormComponent],
+  imports: [HeaderComponent, MainContentComponent, TaskFormComponent, TaskCommentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
