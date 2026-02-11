b73c59490e66774fee4a247c573b1177b6c8721c
pcampina <pablo@campina.dev>
2026-02-11 09:04:15 +0000

    chore: setup cdk drag and drop

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,23 +1,48 @@
 import { Component, inject } from '@angular/core';
 import { TaskCardComponent } from "../task-card/task-card.component";
 import { TaskService } from '../../services/task.service';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
+import { ITask } from '../../interfaces/task.interface'
 
 @Component({
   selector: 'app-task-list',
-  imports: [TaskCardComponent],
+  imports: [TaskCardComponent, CdkDropList, CdkDrag],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
+  todoTasks: ITask[] = []
+  doingTasks: ITask[] = []
+  doneTasks: ITask[] = []
+
   private readonly _taskService = inject(TaskService);
 
   ngOnInit() {
     this._taskService.todoTasks.subscribe((todoList) => {
-      console.log(todoList)
-
-      todoList[0].name = 'Name changed'
-
-      this._taskService.loadCurrentTodoList();
+      this.todoTasks = todoList;
     });
+
+    this._taskService.doingTasks.subscribe((doingList) => {
+      this.doingTasks = doingList;
+    });
+
+    this._taskService.doneTasks.subscribe((doneList) => {
+      this.doneTasks = doneList;
+    });
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      )
+    }
   }
 }
